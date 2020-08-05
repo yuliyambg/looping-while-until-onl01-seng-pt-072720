1f159579e76a0f1df3b7c3bcb8592b433b880d2a
secure-revert-8990 <yuliyambg@gmail.com>
2020-08-05 00:22:42 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,8 +1,10 @@
 def using_until
   levitation_force = 6
   #your code here
-    
-    until levitation_force = 6
-    puts 
+    levitation_force =0
+    until levitation_force == 6
+    puts 'Wingardium Leviosa'
+    levitation_force +=1
+  end
 end
 
